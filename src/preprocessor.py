fd0c0c2e7dac0396ddd7f4124361ab29f54e4874
Patryk Pol <patryk22082005@gmail.com>
2026-03-08 15:49:43 +0100

    preprocessing almost done

---
+++ src/preprocessor.py
@@ -0,0 +1,110 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, LabelEncoder, MinMaxScaler, RobustScaler
+
+
+class DataPreprocessor:
+    def __init__(self, df, config = None):
+        self.raw_data = df
+        self.preprocessed_data = self.raw_data.copy()
+        self.X_train, self.X_test, self.y_train, self.y_test = None, None, None, None
+
+        self.config = config or {}
+
+        #config params and defaults
+        self.categorical_missing_method = self.config.get('categorical_missing_method', 'mode')
+        self.numerical_missing_method = self.config.get('numerical_missing_method', 'median')
+        self.outlier_method = self.config.get('outlier_method', 'cap')
+        self.binary_encoding_method = self.config.get('binary_encoding', 'label')
+        self.categorical_encoding_method = self.config.get('categorical_encoding', 'onehot')
+        self.scaling_method = self.config.get('scaling', 'standard')
+
+        self.missing_threshold = self.config.get('missing_threshold', 0.5)
+        self.random_state = self.config.get('random_state', 0)
+        self.test_size = self.config.get('test_size', 0.2)
+        self.target_col = self.config.get('target_col', None)
+
+        # mappings for config
+        self.scaler_mapping = {
+            'standard': StandardScaler(),
+            'minmax': MinMaxScaler(),
+            'robust': RobustScaler()
+        }
+        self.categorical_missing_method_mapping = {
+            'mode': lambda col: col.mode()[0] if not col.mode().empty else None,
+        }
+        self.numerical_missing_method_mapping = {
+            'median': lambda col: col.median(),
+            'mean': lambda col: col.mean(),
+        }
+
+        self.scaler = self.scaler_mapping[self.scaling_method] or None
+        self.categorical_missing_method_pointer = self.categorical_missing_method_mapping[self.categorical_missing_method]
+        self.numerical_missing_method_pointer = self.numerical_missing_method_mapping[self.numerical_missing_method]
+
+    def preprocess(self):
+        self.handle_missing_values()
+        self.handle_outliers()
+        self.encode_non_numeric_data()
+        self.split_data()
+        self.scale_data()
+
+    def handle_missing_values(self):
+        for col in self.raw_data.columns:
+            if self.raw_data[col].isnull().sum() > 0:
+                if self.raw_data[col].dtype in ['object', 'category']:
+                    val = self.categorical_missing_method_pointer(self.raw_data[col])
+                    self.preprocessed_data[col].fillna(val, inplace=True)
+                else:
+                    val = self.numerical_missing_method_pointer(self.raw_data[col])
+                    self.preprocessed_data[col].fillna(val, inplace=True)
+
+
+    def handle_outliers(self):
+        columns = self.raw_data.select_dtypes(include=[np.number]).columns
+
+        for col in columns:
+            Q1 = self.preprocessed_data[col].quantile(0.25)
+            Q3 = self.preprocessed_data[col].quantile(0.75)
+            IQR = Q3 - Q1
+            lower_bound = Q1 - 1.5 * IQR
+            upper_bound = Q3 + 1.5 * IQR
+
+            self.preprocessed_data[col] = self.preprocessed_data[col].clip(lower_bound, upper_bound)
+
+    def encode_non_numeric_data(self):
+        categorical_cols = self.preprocessed_data.select_dtypes(include=['object', 'category']).columns
+
+        for col in categorical_cols:
+            if self.preprocessed_data[col].nunique() == 2:
+                # binary data: Label Encoding
+                le = LabelEncoder()
+                self.preprocessed_data[col] = le.fit_transform(self.preprocessed_data[col].astype(str))
+            else:
+                # multi-category data: one-hot encoding
+                self.preprocessed_data = pd.get_dummies(
+                    self.preprocessed_data,
+                    columns=[col],
+                    drop_first=True,
+                    prefix=col
+                )
+
+    def split_data(self):
+        y = self.preprocessed_data[self.target_col]
+        X = self.preprocessed_data.drop(self.target_col, axis=1)
+
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
+            X,
+            y,
+            test_size=self.test_size,
+            random_state=self.random_state
+        )
+
+    def scale_data(self):
+        self.scaler.fit(self.X_train)
+        self.X_train = self.scaler.transform(self.X_train)
+        self.X_test = self.scaler.transform(self.X_test)
+
+
+
